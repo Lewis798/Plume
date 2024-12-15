8099f562abdebf0130707e67607bca887b020e82
Lewis798 <QA2160073500@outlook.com>
2024-12-15 20:14:40 +0800

    提交修改

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -10,10 +10,10 @@ export const navbar = defineNavbarConfig([
      items: [
        
        { text: 'C++', link: '/notes/C++/', icon: 'vscode-icons:file-type-cpp3'},
-       { text: '算法', link: '/notes/算法/', icon: 'vscode-icons:file-type-libreoffice-draw' },
-       { text: '计算机网络', link: '/notes/计算机网络/', icon: 'devicon:reactnavigation' },
+       { text: '算法', link: '/notes/Algo/', icon: 'vscode-icons:file-type-libreoffice-draw' },
+       { text: '计算机网络', link: '/notes/Network/', icon: 'devicon:reactnavigation' },
        { text: 'linux', link: '/notes/liunx/', icon: 'skill-icons:linux-light' },
-       { text: '工具百宝箱', link: '/notes/Tools & Skills/', icon: 'unjs:confbox' },
+       { text: '工具百宝箱', link: '/notes/Tools Skills/', icon: 'unjs:confbox' },
      ]
    },
 ])
